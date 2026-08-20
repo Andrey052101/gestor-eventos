ee5bbb113d2b4f81d21cda619df6bea67b79a4ec
Andrey Chaves <andreychaves2cb@gmail.com>
2026-08-20 16:42:46 -0500

    Estructura inicial del proyecto Spring Boot / Maven

---
+++ gestor-eventos/src/test/java/edu/instituto/gestor_eventos/GestorEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.instituto.gestor_eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
